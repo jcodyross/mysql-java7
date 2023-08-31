888fa8963149fd1e985d2e14876109a1769944d6
Cody Ross <cody.ross9614@gmail.com>
2023-08-31 14:13:27 -0400

    week 7

---
+++ src/main/java/projects/ProjectsApp.java
@@ -0,0 +1,17 @@
+package projects;
+
+import projects.dao.DbConnection;
+/**
+ * 
+ */
+public class ProjectsApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DbConnection.getConnection();
+	}
+
+}
